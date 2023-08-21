cc0a99570ff1bf587aee097459d4e3adf6159283
Buzhak <Byzhak@ya.ru>
2023-08-21 18:17:42 +0300

    fix flake8

--- app/api/endpoints/__init__.py
+++
@@ -1,3 +0,0 @@
-from .user import router as user_router
-from .charity_project import router as charityproject_router
-from .donation import router as donation_router
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import (
-    user_router, charityproject_router, donation_router
-)
-
+from app.api.endpoints.user import router as user_router
+from app.api.endpoints.charity_project import router as charityproject_router
+from app.api.endpoints.donation import router as donation_router
 
 main_router = APIRouter()
 main_router.include_router(user_router)
